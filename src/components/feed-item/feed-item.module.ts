8c7305a2d2408f7b1221623e9848d4e4edd38715
blue_lim <chinapoopoo@gmail.com>
2018-01-14 02:46:53 +0900

    피드 아이템 수정

--- src/components/feed-item/feed-item.module.ts
+++ src/components/feed-item/feed-item.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedItemComponent } from './feed-item';
+import { SquareModule } from '../../directive/square/sqare.module';
 
 @NgModule({
     declarations: [ FeedItemComponent ],
-    imports: [ CommonModule ],
+    imports: [ CommonModule , SquareModule],
     exports: [ FeedItemComponent ],
     providers: [],
 })
