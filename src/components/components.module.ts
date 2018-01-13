2acf1b9374002038d970aae8bee3c9f2ecad41de
blue_lim <chinapoopoo@gmail.com>
2018-01-14 02:13:08 +0900

    쓸데업네

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FeedItemComponent } from './feed-item/feed-item';
-@NgModule({
-	declarations: [FeedItemComponent],
-	imports: [],
-	exports: [FeedItemComponent]
-})
-export class ComponentsModule {}
