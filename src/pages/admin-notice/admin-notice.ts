bd4ac8070da53c409b40f866fde1d0095c9f9350
blue_lim <chinapoopoo@gmail.com>
2018-02-05 21:43:13 +0900

    티밍

--- src/pages/admin-notice/admin-notice.ts
+++ src/pages/admin-notice/admin-notice.ts
@@ -10,11 +10,10 @@ import { AdminNoticeWritePage } from '../admin-notice-write/admin-notice-write';
   templateUrl: 'admin-notice.html',
 })
 export class AdminNoticePage {
-  title:string="";
-  notice_txt: string = "";
-  limit=5;
-  offset=0;
-  notice_list: Array<any>
+  limit: number = 5;
+  offset: number = 0;
+  notice_list: Array<any> = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams,private http:HttpService) {
     this.load();
   }
@@ -22,40 +21,26 @@ export class AdminNoticePage {
   ionViewDidLoad() {
     this.load();
   }
-
-  get noticeModel() {
-    return this.notice_txt;
-  }
-  set noticeModel(val) {
-    this.notice_txt = val;
-  }
-
-  // 서버 저장하기 전에 /n -> <br/>로 변환시킨 값을 저장.
-  getNoticeTxt() {
-    return this.notice_txt.replace(/\r\n|\r|\n/g,"<br/>");
-  }
-
-  // 서버에서 불러와서 수정해야하는 상황일 때, <br/> -> /n으로 변환
-  setNoticeTxt(txt: string) {
-    return this.notice_txt = txt.replace(/<br\s?\/?>/g,"\n");
-  }
   
-  load(){
+  load() {
     this.http.get(`/notice?limit=5&offset=${this.offset}`)
     .subscribe(data =>{
       this.notice_list = data.json();
-    })
+    });
   }
-  goDetail(item){
-    this.navCtrl.push(AdminNoticeUpdatePage,{item:item})
+
+  goDetail(item) {
+    this.navCtrl.push(AdminNoticeUpdatePage, {item:item});
   }
-  write(){
-    this.navCtrl.push(AdminNoticeWritePage)
+
+  write() {
+    this.navCtrl.push(AdminNoticeWritePage);
   }
-  delete(notice_no){
+
+  delete(notice_no) {
     this.http.delete(`/notice?notice_no=${notice_no}`)
     .subscribe(data =>{
       this.load();
-    })
+    });
   }
 }
